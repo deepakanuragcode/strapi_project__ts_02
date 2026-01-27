0b81fa1aba182bd9656b45f64394ecdf71359d47
dkag525 <146333126+dkag525@users.noreply.github.com>
2026-01-27 09:10:04 +0530

    save in branch

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+// export default () => ({});
+
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        secure: true,
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "deepak.anurag@rhombuz.io",
+        defaultReplyTo: "deepak.anurag@rhombuz.io",
+      },
+    },
+  },
+  // ...
+});
